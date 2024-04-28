222aa4572d0ae06aa5ed597d6afbdfbbec270534
digital <didev@dinid.net>
2024-04-28 21:48:24 +0200

    refactor(generator): add dedicated stage with workflow for data fiddling
    the data from the nautobot needs to be fiddled a bit before it can
    be used in the render. for example, a serial number and hostname gets
    defined for every device. additionally, a usecase string is assembled
    from the device role, type, and manufacturer. depending on the usecase
    a different template gets selected, which may requires changes to the
    context.
    
    co-authored by raven <raven@mwsq.de>

--- gpncfg/generator/__init__.py
+++ gpncfg/generator/__init__.py
@@ -25,61 +25,108 @@ class Generator:
         self.cfg = cfg
         self.data = data
 
-        self.context = {}
-        self.context["config"] = self.cfg.__dict__
-        self.context["vlan_list"] = data["vlan_list"]
-
-        trans = str.maketrans(" ", "-", "()")
-        for vlan in range(len(self.context["vlan_list"])):
-            self.context["vlan_list"][vlan]["name"] = self.context["vlan_list"][vlan][
-                "name"
-            ].translate(trans)
-
         self.j2 = jinja2.Environment(
             loader=jinja2.FileSystemLoader(searchpath=[get_template_path()]),
             trim_blocks=True,
             lstrip_blocks=True,
         )
 
-    def generate(self):
-        log.info("generating configs")
-        template = self.j2.get_template("switch_arista.j2")
-
+    def fiddle(self):
+        log.info("fiddling data")
         ts = (
             datetime.datetime.utcnow()
             .replace(microsecond=0, tzinfo=datetime.timezone.utc)
             .isoformat()
         )
+        self.sanetize_vlans()
+        self.fiddle_devices(ts)
 
-        for netbox in self.data["device_list"]:
-            extra = dict()
+    def sanetize_vlans(self):
+        trans = str.maketrans(" ", "-", "()")
+        for vlan in self.data["vlan_list"]:
+            vlan["name"] = vlan["name"].translate(trans)
 
-            if (name := netbox["name"]) != None:
-                extra["hostname"] = name
+    def fiddle_devices(self, ts):
+        for device in self.data["device_list"]:
+            # set usecase and device id
+            if device["serial"] == "":
+                device["serial"] = "fallback-serial-" + device["id"]
+
+            device["usecase"] = (
+                device["role"]["slug"]
+                + "_"
+                + device["device_type"]["manufacturer"]["slug"]
+                + "_"
+                + device["device_type"]["slug"]
+            )
+            usecase = device["usecase"]
+
+            # log what we are doing
+            log.debug("fiddling config for serial {}'".format(device["serial"]))
+
+            # add general stuff
+            if (name := device["name"]) != None:
+                device["hostname"] = name
             else:
-                extra["hostname"] = "device-" + netbox["id"]
+                device["hostname"] = "device-" + device["id"]
 
-            if netbox["serial"] == "":
-                extra["serial"] = "fallback-serial-" + netbox["id"]
+            if device["location"] == None:
+                device["snmp_location"] = self.cfg.snmp_location
             else:
-                extra["serial"] = netbox["serial"]
+                device["snmp_location"] = device["location"]["name"]
 
-            if netbox["location"] == None:
-                extra["snmp_location"] = self.cfg.snmp_location
-            else:
-                extra["snmp_location"] = netbox["location"]["name"]
+            device["motd"] = self.cfg.motd.format(timestamp=ts)
 
-            for iface in netbox["interfaces"]:
+            for iface in device["interfaces"]:
                 tagged = [str(vlan["vid"]) for vlan in iface["tagged_vlans"]]
                 if len(tagged) != 0:
                     iface["tagged_vlans"] = ",".join(tagged)
                 else:
                     iface["tagged_vlans"] = "none"
 
-            extra["motd"] = self.cfg.motd.format(timestamp=ts)
+            # add data based on usecase
+            if usecase == "switch_arista_sampelModel":
+                print("doing some stuff")
 
-            ctx = copy.deepcopy(self.context)
-            ctx["netbox"] = netbox
-            ctx["extra"] = extra
+            elif usecase == "switch_arista_1234":
+                print("doing other stuff")
 
-            self.configs[extra["serial"]] = template.render(ctx)
+    def generate(self):
+        log.info("generating configs")
+
+        missing_templates = []
+
+        for device in self.data["device_list"]:
+            usecase = device["usecase"]
+
+            log.debug(
+                "generating config for serial {serial}".format(
+                    name=device["name"],
+                    serial=device["serial"],
+                    usecase=usecase,
+                )
+            )
+
+            context = dict()
+            context["config"] = self.cfg.__dict__
+            context["vlan_list"] = self.data["vlan_list"]
+            context["device"] = device
+
+            try:
+                template = self.j2.get_template(usecase + ".j2")
+                self.configs[device["serial"]] = template.render(context)
+            except jinja2.TemplateNotFound as e:
+                log.warn(
+                    "failed to find template {} for device {}".format(
+                        usecase, device["serial"]
+                    )
+                )
+                if not usecase in missing_templates:
+                    missing_templates.append(usecase)
+
+        if missing_templates != []:
+            log.error(
+                "failed to load templates for these usecases: {}".format(
+                    ", ".join(missing_templates)
+                )
+            )
