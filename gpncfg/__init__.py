9c1c4d93721a8dc699101b10e463372a71aca8f8
digital <didev@dinid.net>
2024-05-11 01:57:27 +0200

    feat(gpncfg): create logging handler with colored formatting

--- gpncfg/__init__.py
+++ gpncfg/__init__.py
@@ -2,8 +2,33 @@
 
 import logging
 
+import colorlog
+
 from .data_provider import DataProvider
 from .generator import Generator
 from .main_action import MainAction, run
 
 log = logging.getLogger(__name__)
+
+
+def color_handler():
+    handler = colorlog.StreamHandler()
+    handler.setFormatter(
+        colorlog.ColoredFormatter(
+            "%(log_color)s%(levelname)s:%(name)s:%(reset)s%(message_log_color)s%(message)s",
+            log_colors={
+                "DEBUG": "light_black",
+                "INFO": "green",
+                "WARNING": "yellow",
+                "ERROR": "red",
+                "CRITICAL": "red",
+            },
+            secondary_log_colors={
+                "message": {
+                    "DEBUG": "light_black",
+                    "CRITICAL": "red",
+                }
+            },
+        )
+    )
+    return handler
