64ec73803141d87e58b3e2a7ec9d7e7ade6e5aaa
digital <didev@dinid.net>
2024-05-29 13:28:08 +0200

    feat(render): implement symlinkless template handling

--- gpncfg/render/__init__.py
+++ gpncfg/render/__init__.py
@@ -11,6 +11,14 @@ import jinja2
 
 log = logging.getLogger(__name__)
 
+TEMPLATE_MAP = {
+    "access-switch_juniper_ex2200c-12t": "access-juniper-ex2_00c.j2",
+    "access-switch_juniper_ex2300c-12p": "access-juniper-ex2_00c.j2",
+    "access-switch_juniper_ex3300-24t": "access-juniper-ex3300.j2",
+    "access-switch_juniper_ex3300-48p": "access-juniper-ex3300.j2",
+    "core-switch_mellanox_sn2410": "json",
+}
+
 
 def get_template_path():
     cur_dir = os.path.dirname(__file__)
@@ -53,7 +61,7 @@ class Renderer:
 
         configs = dict()
 
-        missing_templates = []
+        missing_usecases = set()
 
         for device in data["devices"]:
             usecase = device["usecase"]
@@ -67,30 +75,34 @@ class Renderer:
             )
             cwc = Conglomerate(self.cfg, data["vlans"], device)
 
-            if usecase == "core-switch_mellanox_sn2410":
+            template_name = TEMPLATE_MAP.get(usecase)
+            if not template_name:
+                log.warn(
+                    "failed to find template for usecase {} when rendering {nodename} {serial}".format(
+                        usecase, **device
+                    )
+                )
+                missing_usecases.add(usecase)
+            elif template_name == "json":
                 cwc.set_config(
                     json.dumps([{"set": device["config"]}], indent=2, sort_keys=True)
                 )
             else:
                 try:
-                    template = self.j2.get_template(usecase + ".j2")
+                    template = self.j2.get_template(template_name)
                     cwc.set_config(template.render(cwc.context))
                 except jinja2.TemplateNotFound as e:
-                    log.warn(
-                        "failed to find template {} for device {}".format(
-                            usecase, device["serial"]
+                    raise Exception(
+                        "failed to find jinja2 template {} for usecase {} for device {serial} {nodename}".format(
+                            template_name, usecase, **device
                         )
                     )
-                    if not usecase in missing_templates:
-                        missing_templates.append(usecase)
 
             configs[device["id"]] = cwc
 
-        if missing_templates != []:
+        if missing_usecases:
             log.error(
-                "failed to load templates for these usecases: {}".format(
-                    ", ".join(missing_templates)
-                )
+                f"failed to load templates for these usecases: {missing_usecases}"
             )
 
         return configs
