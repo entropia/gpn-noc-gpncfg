492f76c837ae74a1dad5eaf2a330ffb93fd1b6e5
Markus Hauschild <markus@moepman.eu>
2025-06-18 08:55:26 +0200

    statistics: implement starting/stopping the web server

--- gpncfg/statistics/__init__.py
+++ gpncfg/statistics/__init__.py
@@ -1,8 +1,9 @@
 import logging
 from enum import Enum
-from typing import Dict
+from threading import Thread
+from typing import Dict, Optional
 
-from prometheus_client import Gauge
+from prometheus_client import Gauge, start_http_server
 
 log = logging.getLogger(__name__)
 
@@ -18,6 +19,8 @@ class StatisticsType(Enum):
 class Statistics(object):
     _instance = None
     _data: Dict[StatisticsType, Gauge] = {}
+    _server = None
+    _server_thread: Optional[Thread] = None
 
     def __new__(cls):
         if not isinstance(cls._instance, cls):
@@ -29,6 +32,20 @@ class Statistics(object):
                     ["device"],
                 )
         return cls._instance
+
+    def start_http_server(self, port: int) -> None:
+        if not self._server:
+            self._server, self._server_thread = start_http_server(port)
+        else:
+            logging.warning(f"HTTP server already started, not starting a second time")
+
+    def stop_http_server(self, wait: bool) -> None:
+        if not self._server:
+            logging.warning(f"HTTP not started, ignoring stop request")
+        else:
+            self._server.shutdown()
+            if wait and self._server_thread:
+                self._server_thread.join()
 
     def update(self, device_slug: str, action: StatisticsType) -> None:
         self._data[action].labels(device_slug).set_to_current_time()
